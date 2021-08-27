74d116d27f190915c9d598cf6e487f42456c4511
sxeix <sxeixarts@gmail.com>
2021-08-28 00:20:53 +0100

    Large refactoring and restyling, intro of header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { AnimeNameListComponent } from './anime-name-list/anime-name-list.component';
 import { FormsModule } from '@angular/forms';
 import { AnimeStatusDropdownComponent } from './anime-status-dropdown/anime-status-dropdown.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AnimeNameListComponent,
-    AnimeStatusDropdownComponent
+    AnimeStatusDropdownComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
